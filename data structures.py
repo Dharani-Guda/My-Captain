c1e9673bb60797287c1fb3b72046074882202ebc
GudaDharani <67778272+GudaDharani@users.noreply.github.com>
2020-07-05 20:13:39 +0530

    Create data structures.py

---
+++ data structures.py
@@ -0,0 +1,7 @@
+l=[1,"a",False]#l is list
+t={1,2,"b",True}#t is tuple
+d={"a":1,"b":2,"c":3}#d is dictionary
+add=t[0]+t[1]#accessing tuples
+print(add)
+d.remove("a")#removing elements in dictionary
+print(d)
